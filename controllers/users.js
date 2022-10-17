1dc7c75558c79a259047966e133851bb2fb7bb5e
NikitaSavchuk97 <99026028+NikitaSavchuk97@users.noreply.github.com>
2022-10-17 20:32:45 +0300

    ...

--- controllers/users.js
+++ controllers/users.js
@@ -26,8 +26,8 @@ module.exports.getUserById = (req, res) => {
 		.catch((err) => {
 			if (err.name === 'CastError') {
 				res.status(400).send({ message: 'Переданы некорректные данные' });
-			} else if (err.name === 'NotFound') {
-				res.status(404).send({ message: 'Пользователя с таким _id не существует' });
+			} else if (err.message === 'NotFound') {
+				res.status(404).send({ message: 'Пользователь по указанному _id не найден' });
 			} else {
 				res.status(500).send({ message: 'На сервере произошла ошибка' });
 			}
