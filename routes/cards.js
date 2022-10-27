5d78b2173182b70e29520e5384e86381cc810c3c
NikitaSavchuk97 <99026028+NikitaSavchuk97@users.noreply.github.com>
2022-10-27 14:38:56 +0300

    я устал, хочу пиво 8

--- routes/cards.js
+++ routes/cards.js
@@ -1,14 +1,16 @@
 const router = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
-const { createCard, getCards, deleteCardById, likeCard, dislikeCard } = require('../controllers/cards');
+const {
+  createCard, getCards, deleteCardById, likeCard, dislikeCard
+} = require('../controllers/cards');
 
 router.get('/cards', getCards);
 
 router.post('/cards', celebrate({
-	body: Joi.object().keys({
-		name: Joi.string().required().min(2).max(30),
-		link: Joi.string().required().pattern(/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w.-]+)+[\w\-._~:/?#[\]@!$&'()*+,;=.]+$/),
-	}),
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required().pattern(/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w.-]+)+[\w\-._~:/?#[\]@!$&'()*+,;=.]+$/),
+  }),
 }), createCard);
 
 router.put('/cards/:cardId/likes', likeCard);
