5c508159d40581ec598075304a198273ba7e372a
NikitaSavchuk97 <99026028+NikitaSavchuk97@users.noreply.github.com>
2022-10-28 12:40:02 +0300

    ура

--- middlewares/serverError.js
+++ middlewares/serverError.js
@@ -8,14 +8,3 @@ module.exports = (err, req, res, next) => {
 
   next();
 };
-
-module.exports = (err, req, res, next) => {
-  const { statusCode = 404, message } = err;
-  res
-    .status(statusCode)
-    .send({
-      message: statusCode === 404 ? 'Такой страницы не существует' : message,
-    });
-
-  next();
-};
